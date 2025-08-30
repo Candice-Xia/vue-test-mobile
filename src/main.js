dc6356d071629aa7403e10b19db063ede9b09658
Candice Xia <Candice_Xia@Kingston.com.cn>
2025-08-30 20:35:54 +0800

    init commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import vuetify from './plugins/vuetify'
+import App from './App.vue'
+
+createApp(App)
+     .use(vuetify)
+     .mount('#app')
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,16 @@
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi', // This is already the default value - only for display purposes
+  },
+  components,
+  directives,
+})
+export default vuetify
+
